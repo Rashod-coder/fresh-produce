962a3e7a37c13ad5b4655595154d725b6037798f
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-05-18 13:07:57 -0700

    optimized big devices

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import backgroundImage from '../Assets/pineapple-supply-co-kjTQwE-Q50M-unsplash.jpg'; 
-import backGroundI from '../Assets/brooke-lark-3A1etBW5cBk-unsplash.jpg';
-import backGroundi from '../Assets/amy-shamblen-bqjyFm32HDM-unsplash.jpg'
+import backgroundImage from '../Assets/tirza-van-dijk-dq0x8AvNKv8-unsplash.jpg'; 
+import backGroundI from '../Assets/nery-montenegro-3ak9PMcx048-unsplash.jpg';
+import backGroundi from '../Assets/masahiro-naruse-4Hmf3K6NRQY-unsplash.jpg'
 
 function Home() {
   const backgroundStyle = {
@@ -48,33 +48,38 @@ function Home() {
       <div style={headerStyle}>
         <h1 className='text-light text-center' style={{fontFamily: 'montserrat'}}> Welcome to Fresh Market Hub.</h1>
       </div>
-      <div id="carouselExampleAutoplaying" className="carousel carousel-dark slide" data-bs-ride="carousel ">
+      <div id="carouselExampleAutoplaying" className="carousel carousel-dark slide" data-bs-ride="carousel" data-bs-interval="2500">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="2500">
             <img src={backgroundImage} style={{height: '90vh'}} className="d-block w-100" alt="Slide 1" />
-            <div class="carousel-caption  d-md-block">
-        <h5>A full on digital farmers market nothing like others!</h5>
-        <a href="/register" 
-          style={buttonStyle}
-          onMouseOver={e => e.currentTarget.style.backgroundColor = buttonHoverStyle.backgroundColor}
-          onMouseOut={e => e.currentTarget.style.backgroundColor = buttonStyle.backgroundColor}>
-          Get Started Today
-        </a>
-      </div>
+            <div className="carousel-caption d-md-block">
+              <h5>A full on digital farmers market nothing like others!</h5>
+              <a href="/register" 
+                style={buttonStyle}
+                onMouseOver={e => e.currentTarget.style.backgroundColor = buttonHoverStyle.backgroundColor}
+                onMouseOut={e => e.currentTarget.style.backgroundColor = buttonStyle.backgroundColor}>
+                Get Started Today
+              </a>
+            </div>
           </div>
           <div className="carousel-item" data-bs-interval="2500">
             <img src={backGroundI} style={{height: '90vh'}} className="d-block w-100" alt="Slide 2" />
-            <div class="carousel-caption  d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+            <div className="carousel-caption d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
           </div>
           <div className="carousel-item" data-bs-interval="2500">
             <img src={backGroundi} style={{height: '90vh'}} className="d-block w-100" alt="Slide 3" />
-            <div class="carousel-caption  d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+            <div className="carousel-caption d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
