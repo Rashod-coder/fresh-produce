d74961b2de3dec528e90ce64b0a4ca7ca133e142
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-05-25 14:13:13 -0700

    changes made

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,12 +3,13 @@ import { collection, getDocs, query, where, deleteDoc, doc, getDoc, updateDoc } 
 import { db, auth } from '../Firebase/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Cart() {
     const [cartItems, setCartItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [user, setUser] = useState(null);
-    const [forceRender, setForceRender] = useState(false); // State variable to trigger re-render
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -45,13 +46,10 @@ function Cart() {
         try {
             await deleteDoc(doc(db, "cart", itemId));
             console.log("Item removed from cart:", itemId);
-    
             fetchCartItems(user.uid);
 
-            // Update cartItems state after removing the item
             const updatedCartItems = cartItems.filter(item => item.id !== itemId);
             setCartItems(updatedCartItems);
-
         } catch (error) {
             console.error("Error removing item from cart:", error);
         }
@@ -66,66 +64,47 @@ function Cart() {
     };
 
     const handlePaymentSuccess = async (itemId, quantity, cart) => {
-        console.log("Item ID:", itemId); // Log the itemId
-
         try {
-            // Update the stock of the product
             const itemRef = doc(db, "store", itemId);
-            console.log("Item ID:", itemId); // Log the itemId
             const itemSnapshot = await getDoc(itemRef);
-            console.log("itemSnapshot:", itemSnapshot);
-            
             const currentItem = itemSnapshot.data();
-            console.log("currentItem:", currentItem);
-    
-            // Check if currentItem is defined
+
             if (!currentItem) {
                 console.error("Item data not found");
                 return;
             }
-    
+
             const updatedStock = currentItem.quantity - quantity;
-    
+
             if (updatedStock <= 0) {
-                // If the stock becomes zero, mark the product as out of stock
                 await updateDoc(itemRef, { quantity: 0, status: "Out of Stock" });
             } else {
-                // Otherwise, update the stock with the new quantity
                 await updateDoc(itemRef, { quantity: updatedStock });
             }
-    
-            // Remove the item from the cart
-            await removeFromCart(cart);
-            setForceRender(prev => !prev);
 
-    
-            // Optionally, you can update the cart items state
-            const updatedCartItems = cartItems.filter(cartItem => cartItem.id !== itemId);
-            setCartItems(updatedCartItems);
-    
-            // Show alert or any other action
+            await removeFromCart(cart);
+            setCartItems(cartItems.filter(cartItem => cartItem.id !== itemId));
+
             alert('Transaction completed successfully!');
         } catch (error) {
             console.error("Error updating stock:", error);
-            // Handle error
         }
     };
 
     const onPaymentApprove = (itemId, quantity, cart) => (data, actions) => {
         return actions.order.capture().then(async details => {
-            // Call handlePaymentSuccess to update stock and remove item from cart
             await handlePaymentSuccess(itemId, quantity, cart);
-        
-            // Close PayPal window
-            actions.restart();
+            actions.close();
         });
     };
 
     return (
-        <div className="container-fluid mt-5" style={{ minHeight: '100vh' }}>
-            <div className="row">
-                <div className="col-md-8 border-end">
-                    <h1>Your Cart</h1>
+        <div className="container-fluid mt-5 d-flex justify-content-center" style={{ minHeight: '100vh' }}>
+            <div className="row w-100">
+                <div className="col-12">
+                    <h1 style={{ fontSize: '2rem', textAlign: 'center' }}>Shopping Cart</h1>
+                    <hr style={{ borderTop: '1px solid #ccc' }} />
+
                     {isLoading ? (
                         <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '50vh' }}>
                             <div className="spinner-border" role="status">
@@ -133,51 +112,67 @@ function Cart() {
                             </div>
                         </div>
                     ) : (
-                        <div>
+                        <div className="table-responsive">
                             {cartItems.length > 0 ? (
-                                <ul className="list-group">
-                                    {cartItems.map(item => (
-                                        <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center border-bottom">
-                                            <div>
-                                                <h5>{item.productName}</h5>
-                                                <p>Price: ${parseFloat(item.Price).toFixed(2)} each</p>
-                                                <p>Quantity: {item.quantity} lbs</p>
-                                                <p>Produce Code: {item.Id.substring(0, 8)}</p>
-                                                <p>Produce Code: {item.payee}</p>
-                                                <p>Total: ${(parseFloat(item.Price) * parseInt(item.quantity, 10)).toFixed(2)}</p>
-                                                <PayPalScriptProvider options={{ "client-id": "AZbnZ6MJRL0j1tx5Pa_ZNsMCy_kGlr626jtRg86ZLRB9PiIlJTOCDKKf53X6xZHt9k1X-QIww7uGbQAz" }}>
-                                                    <PayPalButtons
-                                                        createOrder={(data, actions) => {
-                                                            return actions.order.create({
-                                                                purchase_units: [{
-                                                                    amount: {
-                                                                        value: (parseFloat(item.Price) * parseInt(item.quantity, 10)).toFixed(2),
-                                                                    },
-                                                                    payee: {
-                                                                        email_address: item.payee, 
-                                                                    },
-                                                                }],
-                                                            });
-                                                        }}
-                                                        style={{ layout: 'vertical', color: 'blue', shape: 'rect', label: 'pay' }}
-                                                        onApprove={onPaymentApprove(item.Id, parseInt(item.quantity, 10), item.id)}
-                                                    />
-                                                </PayPalScriptProvider>
-                                            </div>
-                                            <button className="btn btn-danger btn-lg" onClick={() => removeFromCart(item.id)}>Remove</button>
-                                        </li>
-                                    ))}
-                                </ul>
+                                <>
+                                    <table className="table" style={{ fontSize: '1.2rem' }}>
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Product</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">SKU</th>
+                                                <th scope="col">PayPal</th>
+                                                <th scope="col">Remove</th> {/* Moved Remove column to the end */}
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {cartItems.map(item => (
+                                                <tr key={item.id}>
+                                                    <td style={{ verticalAlign: 'middle' }}>{item.productName}</td>
+                                                    <td style={{ verticalAlign: 'middle' }}>${parseFloat(item.Price).toFixed(2)}</td>
+                                                    <td style={{ verticalAlign: 'middle' }}>{item.quantity}</td>
+                                                    <td style={{ verticalAlign: 'middle' }}>
+                                                        <span style={{ fontSize: '1.2em', color: '#555' }}>{item.id.substring(0, 8)}</span>
+                                                    </td>
+                                                    <td style={{ verticalAlign: 'middle' }}>
+                                                        <PayPalScriptProvider options={{ "client-id": "AZbnZ6MJRL0j1tx5Pa_ZNsMCy_kGlr626jtRg86ZLRB9PiIlJTOCDKKf53X6xZHt9k1X-QIww7uGbQAz" }}>
+                                                            <PayPalButtons
+                                                                createOrder={(data, actions) => {
+                                                                    return actions.order.create({
+                                                                        purchase_units: [{
+                                                                            amount: {
+                                                                                value: (parseFloat(item.Price) * parseInt(item.quantity, 10)).toFixed(2),
+                                                                            },
+                                                                            payee: {
+                                                                                email_address: item.payee,
+                                                                            },
+                                                                        }],
+                                                                    });
+                                                                }}
+                                                                style={{ layout: 'vertical', color: 'blue', shape: 'rect', label: 'pay', height: 30 }} // Adjust the height as needed
+                                                                onApprove={onPaymentApprove(item.Id, parseInt(item.quantity, 10), item.id)}
+                                                            />
+                                                        </PayPalScriptProvider>
+                                                    </td>
+                                                    <td style={{ verticalAlign: 'middle' }}>
+                                                        <FontAwesomeIcon icon={faTrashAlt} className="text-danger" style={{ marginLeft: '30px', cursor: 'pointer' }} onClick={() => removeFromCart(item.id)} />
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                    <hr style={{ borderTop: '1px solid #ccc' }} />
+                                    <div className="d-flex justify-content-between align-items-center mt-3" style={{ fontSize: '1.5rem' }}>
+                                        <span>Total:</span>
+                                        <span>${calculateTotal()}</span>
+                                    </div>
+                                </>
                             ) : (
-                                <p>Your cart is empty</p>
+                                <p style={{ fontSize: '1.2rem', textAlign: 'center' }}>Your cart is empty</p>
                             )}
                         </div>
                     )}
-                </div>
-                <div className="col-md-4">
-                    <div className="border-start p-3">
-                        <h3 className="mt-3">Total: ${calculateTotal()}</h3>
-                    </div>
                 </div>
             </div>
         </div>
